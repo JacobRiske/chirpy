a31bebd466f9c2d7a8f44ebc7c32eb1711d0456e
Jacob Riske <68482829+JacobRiske@users.noreply.github.com>
2026-09-14 16:48:23 -0700

    Add routing (#2)

--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const (
 	_fileroot = "."
 )
+
+type apiConfg struct {
+	fileSeverHits atomic.Int32
+}
+
+func (cfg *apiConfg) hitcountString(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileSeverHits.Load())))
+}
+
+func (cfg *apiConfg) resetConfg(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileSeverHits.Swap(0)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfg) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileSeverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func headrerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
@@ -16,9 +43,16 @@ func headrerReadiness(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var (
+		apiCfg = &apiConfg{}
+		header = http.StripPrefix("/app", http.FileServer(http.Dir(_fileroot)))
+	)
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(_fileroot))))
-	mux.HandleFunc("/healthz", headrerReadiness)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(header))
+	mux.HandleFunc("GET /metrics", apiCfg.hitcountString)
+	mux.HandleFunc("GET /healthz", headrerReadiness)
+	mux.Handle("POST /reset", apiCfg.resetConfg(http.HandlerFunc(headrerReadiness)))
 
 	srv := &http.Server{
 		Addr:    ":8085",
