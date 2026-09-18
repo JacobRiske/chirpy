66b0a950ce43d91477c9bd2c6435a204b9da855a
Jacob Riske <68482829+JacobRiske@users.noreply.github.com>
2026-09-17 21:45:05 -0700

    built in the achitecture (#3)

--- main.go
+++ main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/pkg/errors"
 )
 
 const (
-	_fileroot = "."
+	_fileroot    = "."
+	_defaultAddr = ":8085"
 )
 
 type apiConfg struct {
@@ -16,10 +20,17 @@ type apiConfg struct {
 }
 
 func (cfg *apiConfg) hitcountString(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("admin.html")
+	if err != nil {
+		http.Error(w, errors.Wrap(err, "could not load in the page").Error(), http.StatusInternalServerError)
+		return
+	}
+
+	htmlFormat := fmt.Sprintf(string(data), cfg.fileSeverHits.Load())
+
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
-	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileSeverHits.Load())))
+	w.Write([]byte(htmlFormat))
 }
 
 func (cfg *apiConfg) resetConfg(next http.Handler) http.Handler {
@@ -44,22 +55,22 @@ func headrerReadiness(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var (
-		apiCfg = &apiConfg{}
-		header = http.StripPrefix("/app", http.FileServer(http.Dir(_fileroot)))
+		apiCfg    = &apiConfg{}
+		appHeader = http.StripPrefix("/app", http.FileServer(http.Dir(_fileroot)))
 	)
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", apiCfg.middlewareMetricsInc(header))
-	mux.HandleFunc("GET /metrics", apiCfg.hitcountString)
-	mux.HandleFunc("GET /healthz", headrerReadiness)
-	mux.Handle("POST /reset", apiCfg.resetConfg(http.HandlerFunc(headrerReadiness)))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(appHeader))
+	mux.HandleFunc("GET /admin/metrics", apiCfg.hitcountString)
+	mux.HandleFunc("GET /api/healthz", headrerReadiness)
+	mux.Handle("POST /admin/reset", apiCfg.resetConfg(http.HandlerFunc(headrerReadiness)))
 
 	srv := &http.Server{
-		Addr:    ":8085",
+		Addr:    _defaultAddr,
 		Handler: mux,
 	}
 
-	log.Printf("Seving on http://localhost%s\n", srv.Addr)
+	log.Printf("Seving on http://localhost%s\n", _defaultAddr)
 
 	log.Fatal(srv.ListenAndServe())
 }
